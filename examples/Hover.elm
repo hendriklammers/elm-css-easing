fe65a52d11421a70063fb839c6c8dfce13663473
Hendrik Lammers <lammers@gmail.com>
2019-05-27 20:41:22 +0700

    Add simple example

---
+++ examples/Hover.elm
@@ -0,0 +1,37 @@
+module Hover exposing (main)
+
+import Css exposing (..)
+import Css.Easing exposing (easeOutExpo, easeOutSine)
+import Css.Transitions as T
+import Html.Styled exposing (div, text, toUnstyled)
+import Html.Styled.Attributes exposing (css)
+
+
+main =
+    toUnstyled <|
+        div
+            [ css
+                [ position relative
+                , backgroundColor <| hex "F0D11F"
+                , width <| px 200
+                , height <| px 100
+                , before
+                    [ position absolute
+                    , display block
+                    , property "content" "''"
+                    , width <| pct 100
+                    , height <| pct 100
+                    , backgroundColor <| hex "01A6D6"
+                    , transform <| scaleX 0
+                    , property "transform-origin" "0 0"
+                    , T.transition
+                        [ T.transform3 500 0 easeOutSine
+                        ]
+                    ]
+                , hover
+                    [ before
+                        [ transform <| scaleX 1 ]
+                    ]
+                ]
+            ]
+            [ text "" ]
